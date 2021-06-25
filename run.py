1f9abff0c6d4044e0328acd7526aefec7b1fbad1
Mark Hil <aleksanderhil@gmail.com>
2021-06-25 15:51:45 +0100

    Read issues from CSV and get issue

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import csv
 
 from dotenv import dotenv_values
 
@@ -12,13 +13,16 @@ domain = config['COMPANY_DOMAIN']
 
 base_url = "https://" + domain + ".net/rest/api/3/issue/"
 auth = HTTPBasicAuth(email, api_token)
-headers = {
-   "Accept": "application/json"
-}
+headers = {"Accept": "application/json"}
 
 def get_issue(issue_id):
     url = base_url + issue_id
     response = requests.request("GET", url, headers=headers, auth=auth)
     print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
 
-get_issue('EL-1409')
+with open('issues.csv', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    # Skip first row
+    next(csv_reader)
+    for row in csv_reader:
+        get_issue(row[0])
