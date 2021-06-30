0e6136333c5696a10d9fb5fbb1f5c483dd2221a4
Mark Hil <aleksanderhil@gmail.com>
2021-06-30 14:52:52 +0100

    Stub open_file function

--- run.py
+++ run.py
@@ -21,10 +21,6 @@ def get_issue(issue_id):
     response = requests.request("GET", url, headers=headers, auth=auth)
     print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
 
-
-
-
-
 def update_issue(issue_id, priority):
     headers = {"Accept": "application/json","Content-Type": "application/json"}
     url = base_url + issue_id
@@ -32,13 +28,15 @@ def update_issue(issue_id, priority):
     response = requests.request("PUT", url, headers=headers, data=payload, auth=auth)
     print(response)
 
+def open_file(file):
+    pass
+
 # with open('issues.csv', newline='') as csvfile:
 #     csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 #     # Skip first row
 #     next(csv_reader)
 #     for row in csv_reader:
-#         get_issue(row[0])
-
-update_issue('EL-1409', 'Low')
+#         update_issue(row[0],row[1].strip())
 
 # get_issue('EL-1409')
+# update_issue('EL-1409', 'Critical')
